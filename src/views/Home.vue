218fdd2c2af228f37829ebbd3131e8b4936e13b5
LucasFrazier <lucasfrazier1@gmail.com>
2019-06-05 11:25:04 -0400

    home looks fine

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <div class="home"> -->
+    <!-- <img src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <!-- </div> -->
+  <section class="hero is-primary is-fullheight">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Players
+      </h1>
+      <h2 class="subtitle">
+        Can you believe it
+      </h2>
+      <a class="button is-large is-fullwidth">Login</a>
+      <a class="button is-large is-fullwidth">Register</a>
+    </div>
   </div>
+</section>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 };
 </script>
+
+<style lang="scss" scoped>
+  .container {
+    // padding: 25px;
+    font-size: 10px;
+  }
+</style>
