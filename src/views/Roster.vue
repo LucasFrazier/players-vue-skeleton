5a84e566f45ba087b4e25594030d8ceb245b436f
LucasFrazier <lucasfrazier1@gmail.com>
2019-06-05 10:59:56 -0400

    roster page looking good

--- src/views/Roster.vue
+++ src/views/Roster.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="roster">
-    <h1>This is the roster page</h1>
+  <div class="roster container">
+    <h1 class="title">Roster</h1>
+    <table class="table">
+    <tr>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Rating</th>
+      <th>Handedness</th>
+      <th></th>
+    </tr>
+    <!-- {this.state.players.map(player => ( -->
+      <tr>
+        <th key={player.id}>Lucas</th>
+        <th>Frazier</th>
+        <th>10</th>
+        <th>right</th>
+        <th>
+          <button 
+            class="delete is-danger" 
+            
+          />
+        </th>
+      </tr>
+      <!-- ))} -->
+    </table>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .container {
+    padding: 25px;
+    font-size: 10px;
+  }
+</style>
